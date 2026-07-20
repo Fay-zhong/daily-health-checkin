1f94919fc847d7e1b8fbf802a624330fa5c1fee0
Fay-zhong <Fay-zhong@users.noreply.github.com>
2026-07-20 11:52:23 +0800

    Add daily calorie balance calculator

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='daily-rhythm-v4';
+const CACHE='daily-rhythm-v5';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
